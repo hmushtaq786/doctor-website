2a1f97cae194a312de215bd498b1f9d5e4ae5712
Hamza <hm.khanzada786@gmail.com>
2020-04-28 10:21:41 +0500

    templates added

--- doctor_app/views.py
+++ doctor_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    html = '''
+    <html>
+    <body>
+    <h2>Hello</h2>
+    </body>
+    </html>
+    '''
+    return HttpResponse(html)
