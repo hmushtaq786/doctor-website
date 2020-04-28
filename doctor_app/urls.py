27fd3f40206eaf1a715833194bd26b6a256d0458
Hamza <hm.khanzada786@gmail.com>
2020-04-28 19:50:45 +0500

    views urls forms added

--- doctor_app/urls.py
+++ doctor_app/urls.py
@@ -3,4 +3,8 @@ from doctor_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
