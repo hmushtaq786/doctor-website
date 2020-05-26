0bcbce3cae01e46da3e40660398f344c556ed733
Hamza <hm.khanzada786@gmail.com>
2020-05-26 15:18:12 +0500

    fix

--- doctor_website/settings.py
+++ doctor_website/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'doctor_app'
+    'doctor_app',
     'cloudinary_storage',
     'cloudinary',
 ]
